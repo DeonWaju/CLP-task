166cd1875a896253d294d3632231c22474a22fc0
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-12-09 22:04:00 +0100

    Update Constants.kt

--- app/src/main/java/com/example/taskcdp/util/Constants.kt
+++ app/src/main/java/com/example/taskcdp/util/Constants.kt
@@ -1,9 +1,5 @@
 import com.example.taskcdp.BuildConfig
 
 object Constants {
-    const val WEATHER_URL = "forecast.json?key=${BuildConfig.API_KEY}"
-    const val SEARCH_CITY_URL = "search.json?key=${BuildConfig.API_KEY}"
-
-
-    const val SOMETHING_WRONG = "Something went wrong, Please check your network..."
+      const val SOMETHING_WRONG = "Something went wrong, Please check your network..."
 }
